28169eca411b35245d89ab0c51f978a91acb41a6
ty1er.durden <pythonclub@loudsquel.ch>
2019-06-04 09:32:42 +0100

    Initial commit

---
+++ pythonclub/main.py
@@ -0,0 +1,30 @@
+import flask
+import flask_caching
+import repo
+
+app = flask.Flask(__name__)
+cache = flask_caching.Cache(app, config={"CACHE_TYPE": "simple"})
+
+
+@cache.cached(timeout=60, key_prefix="get_git_repos")
+def get_git_repos():
+    return [repo["node"] for repo in repo.get_git_repos(max_repos=20)]
+
+
+@app.route("/")
+def index():
+    try:
+        repos = get_git_repos()
+    except Exception:
+        # Yuck.  Ugly hack for error handling.
+        repos = [
+            {
+                "name": "Oops!",
+                "updatedAt": "Cannot retrieve GitHub repositories :(",
+            }
+        ]
+    return flask.render_template("index.html", repos=repos)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ pythonclub/repo.py
@@ -0,0 +1,61 @@
+import json
+import os
+import requests
+
+
+GITHUB_API_URL = "https://api.github.com/graphql"
+GITHUB_API_TOKEN = os.environ["GITHUB_API_TOKEN"]
+
+
+def get_git_repos(max_repos: int = 10):
+    """Gets information pertaining to repositories in a GitHub account
+
+    Args:
+        api_Token (str): GitHub API token (generated as per https://github.com/blog/1509-personal-api-tokens)
+
+    Returns:
+        list: A list of repositories with associated information:
+
+    """
+
+    headers = {"Authorization": "token {t}".format(t=GITHUB_API_TOKEN)}
+    query = (
+        f"{{\n"
+        f"  viewer {{\n"
+        f"  repositories(first: {max_repos}) {{\n"
+        f"      totalCount\n"
+        f"      pageInfo {{\n"
+        f"        hasNextPage\n"
+        f"        endCursor\n"
+        f"      }}\n"
+        f"      edges {{\n"
+        f"        node {{\n"
+        f"          name\n"
+        f"          description\n"
+        f"          updatedAt\n"
+        f"          url\n"
+        f"          releases(first:1) {{\n"
+        f"            nodes {{\n"
+        f"              name\n"
+        f"            }}\n"
+        f"          }}\n"
+        f"        }}\n"
+        f"      }}\n"
+        f"    }}\n"
+        f"  }}\n"
+        f"}}\n"
+    )
+
+    req = requests.post(
+        url=GITHUB_API_URL, json={"query": query}, headers=headers, timeout=5
+    )
+    if req.status_code != 200:
+        raise RuntimeError(
+            "GitHub API query failed. HTTP Error, status {n}: {text}".format(
+                n=req.status_code, text=req.text
+            )
+        )
+    else:
+        result = json.loads(req.text)
+
+    return result["data"]["viewer"]["repositories"]["edges"]
